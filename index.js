348c9d9d7e41fe199f9075a09970a76d50bcc89b
Ehte22 <syedehteshamali22@gmail.com>
2024-11-21 15:29:50 +0530

    domain update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://food-master-r.onrender.com",
     credentials: true
 };
 
